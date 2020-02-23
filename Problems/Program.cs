b69b1a18b0577ce24a32bdd1a4a55f3b7fd62288
Liam Head <kento.nion2012@gmail.com>
2020-02-23 16:23:50 +0200

    Created NUnit test progect and added Problems.Tests to the Solution file

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,35 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = "Hello World!";
+            Console.WriteLine(Prefix(input));
+        }
+
+        static string Prefix(string input){
+            string result = input.Length.ToString() +
+                            ',' +
+                            countWords(input).ToString() +
+                            ':' + 
+                            input;
+
+            return result;
+                                
+        }
+        private static int countWords(string input){
+            int count = 0;
+            for(int i = 0; i < input.Length; i++){
+                if(i > 0 && input[i] == ' ' && input[i-1] == ' '){
+                    continue;
+                }
+                else if(input[i] == ' '){
+                    count++;
+                }
+            }
+            return count;
         }
     }
 }
