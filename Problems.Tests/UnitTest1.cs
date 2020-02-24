8dcec589d92807e7f0a571081a2c0d0988dbcf8b
Liam Head <kento.nion2012@gmail.com>
2020-02-24 21:38:57 +0200

    Relearning the dangers of ctrl+C and ctrl+V still

--- Problems.Tests/UnitTest1.cs
+++ Problems.Tests/UnitTest1.cs
@@ -26,14 +26,14 @@ namespace Problems.Tests
             bool d = (Program.Prefix("what    ...  did you say?? ") == "27,5:what    ...  did you say?? ");
             Assert.That(d, Is.True);
             //extra tests
-            bool d = (Program.Prefix("What is the meaning of ") == "23,5:what    ...  did you say?? ");
-            Assert.That(d, Is.True);
-            bool d = (Program.Prefix("hmmmm, that's an interesting concept...") == "39,5:what    ...  did you say?? ");
-            Assert.That(d, Is.True);
-            bool d = (Program.Prefix("Agreed!") == "7,1:Agreed!");
-            Assert.That(d, Is.True);
-            bool d = (Program.Prefix("") == "27,5:what    ...  did you say?? ");
-            Assert.That(d, Is.True);
+            bool e = (Program.Prefix("What is the meaning of ") == "23,5:what    ...  did you say?? ");
+            Assert.That(e, Is.True);
+            bool f = (Program.Prefix("hmmmm, that's an interesting concept...") == "39,5:what    ...  did you say?? ");
+            Assert.That(f, Is.True);
+            bool g = (Program.Prefix("Agreed!") == "7,1:Agreed!");
+            Assert.That(g, Is.True);
+            bool h = (Program.Prefix("") == "27,5:what    ...  did you say?? ");
+            Assert.That(h, Is.True);
         }
     }
 }
